7634f47e00f60098b40db8c5d37c6bf0668cff8c
Cel <celian_ck@proton.me>
2025-02-25 23:36:59 +0100

    HTTPS en prod + update dependencies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL; // Importer la classe URL
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Forcer HTTPS uniquement en production
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
+        // Précharger les assets avec Vite
         Vite::prefetch(concurrency: 3);
     }
 }
